d0a7f87f4c2efaaa693bc1aecc27770b3ce5ba78
seanhodges61 <seanhodges@cmail.carleton.ca>
2017-06-28 08:54:50 -0400

    waffles

--- Server.js
+++ Server.js
@@ -5,9 +5,9 @@ var app     = express ();
 app.get ("/uset", function (req, res){
 	res.sendFile (path.join (__dirname, "/public/uset/Views/Uset.html"));
 });
- 
+
 app.use (express.static ("./public"));
 
-app.listen (2402, function(){
+app.listen (80, "192.168.123.173", function(){
   console.log ("Still, you feed us lies from the tablecloth"); //TODO
 });
